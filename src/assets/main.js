9388e87ca87640fe0bcac6609dcee916212ccf19
Kevin Ernst <kevin.ernst@mediaman.de>
2016-11-14 16:52:11 +0100

    commit1

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
+ 
+ $.ajax({
+    url: 'https://www.codeschool.com/users/kernst93.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      populateWithCourses(data.courses.completed)
+    }
+  });
 
-  // your code will go here
+  function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
